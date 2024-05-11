10cca3c387611d77e171c09a810b1dc620954582
Rafael Amorim <rafael.amorim@ua.pt>
2024-05-11 22:45:06 +0100

    endPoint Login

--- app/src/main/java/com/example/touristGuide_app/Activities/Login.java
+++ app/src/main/java/com/example/touristGuide_app/Activities/Login.java
@@ -1,15 +1,15 @@
 package com.example.touristGuide_app.Activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -21,10 +21,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
-
     private EditText passwordEditText;
     private EditText mailEditText;
     private boolean isPasswordVisible = false;
+
+    private static final int GOOGLE_LOGIN_REQUEST_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +35,24 @@ public class Login extends AppCompatActivity {
         mailEditText = findViewById(R.id.mail);
         passwordEditText = findViewById(R.id.password);
 
-        MaterialButton loginbtn = findViewById(R.id.loginbtn);
-
-        loginbtn.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.loginbtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String userMail = mailEditText.getText().toString();
                 String userPassword = passwordEditText.getText().toString();
-                // Versão sem endpoint
-                if(userPassword.equals("admin")) {
-                    openMainRoom();
-                } else {
-                    Toast.makeText(Login.this, "Senha incorreta", Toast.LENGTH_SHORT).show();
-                }
-                // Versão com endpoint TROCAR AQUI
-                // sendLoginRequest(userMail, userPassword);
+                //sendLoginRequest(userMail, userPassword);
+                String userId = "1";
+                openMainRoom(userId);
+            }
+        });
+
+        findViewById(R.id.btnGoogle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirecionar para a página de login do Google
+                String googleLoginUrl = "http://192.168.26.112:3000/v1/login";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(googleLoginUrl));
+                startActivityForResult(intent, GOOGLE_LOGIN_REQUEST_CODE);
             }
         });
 
@@ -69,32 +73,39 @@ public class Login extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GOOGLE_LOGIN_REQUEST_CODE && resultCode == RESULT_OK) {
+            // Processar os dados retornados da autenticação do Google
+            if (data != null) {
+                String userId = data.getStringExtra("userId");
+                openMainRoom(userId);
+            }
+        }
+    }
+
     // Método para enviar a solicitação de login
     private void sendLoginRequest(String mail, String password) {
         // Construir o JSON com as credenciais
         JSONObject jsonBody = new JSONObject();
         try {
-            jsonBody.put("clientName", mail); // Aqui você pode ajustar o nome do campo dependendo do que o servidor espera
+            jsonBody.put("clientName", mail);
             jsonBody.put("password", password);
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
         // Enviar a solicitação POST para o servidor
-        // O código para enviar a solicitação e processar a resposta é semelhante ao exemplo anterior
-        // Certifique-se de substituir a URL correta e tratar os erros de resposta
-        
-        // Enviar a solicitação POST para o servidor
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "http://localhost:3000/retrieve-api-key", jsonBody,
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "http://192.168.26.112:3000/retrieve-api-key", jsonBody,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
                             // Processar a resposta do servidor
-                            String userId = response.getString("userId");
+                            String apiKey = response.getString("apiKey");
                             // Abrir a próxima página após o login bem-sucedido
-                            // TROCAR AQUI
-                            // openMainRoom(userId);
+                            openMainRoom(apiKey);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -110,19 +121,13 @@ public class Login extends AppCompatActivity {
         RequestQueueSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 
-    // Versao com EndPoint TROCAR AQUI
-    // // Método para abrir a próxima página após o login bem-sucedido
-    // private void openMainRoom(String userId) {
-    //     Intent intent = new Intent(this, MainActivity.class);
-    //     intent.putExtra("userId", userId);
-    //     startActivity(intent);
-    // }
-    // Versao sem EndPoint
-    private void openMainRoom() {
+    // Método para abrir a próxima página após o login bem-sucedido
+    private void openMainRoom(String userId) {
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("userId", userId);
+        Toast.makeText(this, "Enviou id "+ userId+" para MainActivity", Toast.LENGTH_SHORT).show();
         startActivity(intent);
     }
-
 
     // Método para alternar a visibilidade da senha
     private void togglePasswordVisibility() {
